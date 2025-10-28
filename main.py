7d7c0bb2eb3deecf82c241524a1b3722f2f6a268
magusdere <maximodecima123@gmail.com>
2025-10-28 02:33:54 -0300

    add proveedores module (model + schema + router + CRUD)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routers import producto, categoria
+from routers import producto, categoria, proveedor
 
 app = FastAPI(title="Inventarios API", version="1.0")
 
 # Rutas
 app.include_router(producto.router)
 app.include_router(categoria.router)
+app.include_router(proveedor.router)
 
 @app.get("/")
 def root():
